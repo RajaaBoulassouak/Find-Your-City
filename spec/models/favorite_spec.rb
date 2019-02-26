c15c724de4863528f0ecc8b781a84e4a6099d278
RajaaBoulassouak <raj.b.stretz@gmail.com>
2019-02-25 21:32:24 -0700

    Create Location and Favorites table

---
+++ spec/models/favorite_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Favorite, type: :model do
+  describe 'Relationships' do
+    it { should belong_to :location}
+    it { should belong_to :user }
+  end
+end
