193e7d1f658a125a122c86f56b652e1e6695f94e
RajaaBoulassouak <raj.b.stretz@gmail.com>
2019-02-25 23:03:40 -0700

    Create favorites controller and create favorites action

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,12 @@
+class FavoritesController < ApplicationController
+  before_action :authorize
+  
+  def create
+    location = Location.find_or_create_from_location_params(params[:location])
+    favorite = current_user.favorites.create!(location_id: location.id)
+    if favorite.save
+      redirect_to "/users/#{current_user.id}/favorites"
+      flash[:notice] = "#{location.name} has been added to your Favorites"
+    end
+	end
+end
